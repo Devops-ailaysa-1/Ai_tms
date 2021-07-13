7121c5c69b863bb981b59b64ecd19a9d8a31395b
stephen <stephenrajrc@gmail.com>
2021-07-13 12:49:41 +0530

    added is_active edited paranoid model

--- ai_staff/models.py
+++ ai_staff/models.py
@@ -169,12 +169,13 @@ class Billingunits(ParanoidModel):
         db_table = 'billing_units'
 
 
-class AiUserType(ParanoidModel):
+class AiUserType(models.Model):
     type =models.CharField(max_length=191)
     #unit_code= models.CharField(max_length=191, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True,blank=True, null=True)
     #deleted_at = models.DateTimeField(blank=True, null=True))
+    
     class Meta:
         db_table = 'ai_usertype'
 
@@ -185,6 +186,7 @@ class VendorMemberships(ParanoidModel):
     created_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True,blank=True, null=True)
     #deleted_at = models.DateTimeField(blank=True, null=True))
+    is_active=models.BooleanField(default=True)
     class Meta:
         db_table = 'vendor_membership'
 
@@ -192,5 +194,15 @@ class VendorLegalCategories(ParanoidModel):
     name =models.CharField(max_length=191)
     created_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True,blank=True, null=True)
+    is_active=models.BooleanField(default=True)
     class Meta:
         db_table = 'vendor_legal_categories'
+
+class CATSoftwares(ParanoidModel):
+    name =models.CharField(max_length=191)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True,blank=True, null=True)
+    is_active=models.BooleanField(default=True)
+    class Meta:
+        db_table = 'cat_softwares'
+
