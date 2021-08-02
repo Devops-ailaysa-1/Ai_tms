8d1bc8033049bb22c7e08072c37bd3889ca30668
ThenuVijay <warunvijay@gmail.com>
2021-08-02 18:34:07 +0530

    Primary details for POST job API added,servicerates API changed

--- ai_vendor/models.py
+++ ai_vendor/models.py
@@ -2,10 +2,11 @@ from ai_auth.models import AiUser, user_directory_path
 from ai_staff.models import (Billingunits, CATSoftwares, ContentTypes,
                              Currencies, Languages, MtpeEngines, ParanoidModel,
                              ServiceTypes, SubjectFields,
-                             VendorLegalCategories, VendorMemberships)
+                             VendorLegalCategories, VendorMemberships,Countries)
 from django.db import models
 from django.db.models.constraints import UniqueConstraint
 from ai_auth.models import AiUser,user_directory_path
+from ai_workspace.models import Job
 from ai_staff.models import ContentTypes, Currencies, ParanoidModel, SubjectFields,Languages, VendorLegalCategories,VendorMemberships,MtpeEngines,Billingunits,ServiceTypes,CATSoftwares,ServiceTypeunits
 
 
@@ -101,13 +102,13 @@ class VendorLanguagePair(ParanoidModel):
     created_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True,blank=True, null=True)
     class Meta:
-       unique_together = ("user", "source_lang","target_lang","deleted_at")
+       unique_together = ("user", "source_lang","target_lang")#,"deleted_at")
 
 class VendorServiceInfo(ParanoidModel):
      lang_pair=models.ForeignKey(VendorLanguagePair,related_name='service', on_delete=models.CASCADE)
      mtpe_rate= models.DecimalField(max_digits=5,decimal_places=2 , blank=True, null=True)
      mtpe_hourly_rate=models.DecimalField(max_digits=5,decimal_places=2 , blank=True, null=True)
-     mtpe_count_unit=models.ForeignKey(ServiceTypeunits,related_name='unit_type', on_delete=models.CASCADE)
+     mtpe_count_unit=models.ForeignKey(ServiceTypeunits,related_name='unit_type', on_delete=models.CASCADE, blank=True, null=True)
      created_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
      updated_at = models.DateTimeField(auto_now=True,blank=True, null=True)
 
@@ -144,3 +145,47 @@ class MtpeSamples(ParanoidModel):
     created_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True,blank=True, null=True)
     # deleted_at = models.DateTimeField(auto_now=True,blank=True, null=True)
+
+class AssignedVendors(ParanoidModel):
+    customer= models.ForeignKey(AiUser,related_name='customer',on_delete=models.CASCADE)
+    vendor =  models.ForeignKey(AiUser,related_name='vendor',on_delete=models.CASCADE)
+
+
+
+class jobboard_details(models.Model):
+    job=models.ForeignKey(Job, on_delete=models.CASCADE,related_name="job_detail")
+    service = models.CharField(max_length=191,blank=True, null=True)
+    steps = models.CharField(max_length=191,blank=True, null=True)
+    # src_lang = models.CharField(max_length=191,blank=True, null=True)
+    # tar_lang = models.CharField(max_length=191,blank=True, null=True)
+    sub_field = models.CharField(max_length=191,blank=True, null=True)
+    content_type = models.CharField(max_length=191,blank=True, null=True)
+    proj_name = models.CharField(max_length=191,blank=True, null=True)
+    proj_desc = models.CharField(max_length=1000,blank=True, null=True)
+    bid_deadline = models.DateTimeField(blank=True, null=True)
+    proj_deadline = models.DateTimeField(blank=True, null=True)
+    ven_native_lang = models.ForeignKey(Languages,blank=True, null=True, related_name='vendor_native_lang', on_delete=models.CASCADE)
+    ven_res_country = models.ForeignKey(Countries,blank=True, null=True, related_name='res_country', on_delete=models.CASCADE)
+    ven_special_req = models.CharField(max_length=1000,blank=True, null=True)
+    cust_pc_name = models.CharField(max_length=191,blank=True, null=True)
+    cust_pc_email = models.CharField(max_length=191,blank=True, null=True)
+    rate_range_min = models.DecimalField(
+                         max_digits = 5,
+                         decimal_places = 2,blank=True, null=True)
+    rate_range_max = models.DecimalField(
+                         max_digits = 5,
+                         decimal_places = 2,blank=True, null=True)
+    currency = models.ForeignKey(Currencies,blank=True, null=True, related_name='ven_currency', on_delete=models.CASCADE)
+    unit = models.ForeignKey(Billingunits,blank=True, null=True, related_name='bill_unit', on_delete=models.CASCADE)
+    milestone = models.CharField(max_length=191,blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True,blank=True, null=True)
+
+# class vendor_bid_details(models.Model):
+#     proj_id=models.ForeignKey(jobboard_projdetails, on_delete=models.CASCADE)
+#     bid_amount=models.DecimalField(
+#                          max_digits = 5,
+#                          decimal_places = 2,blank=True, null=True)
+#     vendor= models.ForeignKey(User, on_delete=models.CASCADE)
+#     created = models.DateTimeField(auto_now_add=True)
+#     ven_bid_msg = models.CharField(max_length=1000,blank=True, null=True)
