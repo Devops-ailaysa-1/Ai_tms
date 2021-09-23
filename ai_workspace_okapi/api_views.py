8c0e259778de5fc01d4f1d3233c1b3305f97c8ec
root <maintnandha@gmail.com>
2021-09-23 13:05:03 +0000

    file download name utf-8 support added

--- ai_workspace_okapi/api_views.py
+++ ai_workspace_okapi/api_views.py
@@ -28,7 +28,7 @@ from rest_framework.parsers import JSONParser
 from django.http import  FileResponse
 from rest_framework.views import APIView
 from django.db.models import Q
-
+import  urllib.parse 
 
 logging.basicConfig(filename="server.log", filemode="a", level=logging.DEBUG, )
 
@@ -257,8 +257,10 @@ class DocumentToFile(views.APIView):
                     with open(file_path, 'rb') as fh:
                         response = HttpResponse(fh.read(), content_type=\
                             "application/vnd.ms-excel")
-                        response['Content-Disposition'] = 'attachment; filename='\
-                                    + os.path.basename(file_path)
+                        encoded_filename = urllib.parse.quote(os.path.basename(file_path),\
+                                encoding='utf-8')
+                        response['Content-Disposition'] = 'attachment;filename*=UTF-8\'\'{}'\
+                                            .format(encoded_filename)
                         response["Access-Control-Allow-Origin"] = "*"
                         response["Access-Control-Allow-Headers"] = "*"
                         return response
