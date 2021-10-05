59927d25630b1e7b1418404c6fe9413839362cd7
ilangovenac <acilangoven@gmail.com>
2021-10-05 14:48:08 +0530

    translation status admin added

--- ai_workspace_okapi/admin.py
+++ ai_workspace_okapi/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import MT_Engine
+from .models import MT_Engine,TranslationStatus
 
 # Register your models here.
 admin.site.register(MT_Engine)
+admin.site.register(TranslationStatus)
