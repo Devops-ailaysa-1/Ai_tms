54d40c4b6d2e2298dd61ed07ffa2f75efdf6b629
ThenuVijay <warunvijay@gmail.com>
2021-10-20 11:11:29 +0530

    file size limit extended to 10 mb

--- ai_auth/validators.py
+++ ai_auth/validators.py
@@ -2,6 +2,6 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth import password_validation
 
 def file_size(value): # add this to some file where you can import it from
-    limit = 2 * 1024 * 1024
+    limit = 10 * 1024 * 1024
     if value.size > limit:
-        raise ValidationError('File too large. Size should not exceed 5 MB.')
+        raise ValidationError('File too large. Size should not exceed 10 MB.')
