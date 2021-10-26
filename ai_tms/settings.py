f417402e0922ef97f05c206a91fcafd86097f3ef
ilangovenac <acilangoven@gmail.com>
2021-10-26 14:02:56 +0530

    CORS added for staging

--- ai_tms/settings.py
+++ ai_tms/settings.py
@@ -40,7 +40,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000","http://143.244.140.71", "http://workspacestaging.ailaysa.com", "http://workspace.ailaysa.com",
      "http://workspacetest.ailaysa.com", "http://workspacecommon.ailaysa.com","http://userportal.ailaysa.com",
      "http://transeditor.ailaysa.com", "http://static.ailaysa.com", "https://static.ailaysa.com",
-     "https://userportal.ailaysa.com", "https://traseditor.ailaysa.com"
+     "https://userportal.ailaysa.com", "https://traseditor.ailaysa.com", "https://transeditorstaging.ailaysa.com",
+     "https://userportalstaging.ailaysa.com", "https://staticstaging.ailaysa.com"
 
 ]
 
@@ -49,7 +50,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://143.244.140.71", "http://workspace.ailaysa.com", "http://workspacetest.ailaysa.com",
     "http://workspacecommon.ailaysa.com","http://userportal.ailaysa.com", "http://transeditor.ailaysa.com",
     "http://static.ailaysa.com", "https://static.ailaysa.com","https://userportal.ailaysa.com", 
-    "https://traseditor.ailaysa.com",
+    "https://traseditor.ailaysa.com", "https://transeditorstaging.ailaysa.com", "https://userportalstaging.ailaysa.com",
+    "https://staticstaging.ailaysa.com"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
