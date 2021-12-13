0d181c7e784624465a6f0d48940e56457c5b95dc
ThenuVijay <warunvijay@gmail.com>
2021-12-14 03:52:33 +0530

    Merged with merged manually

--- ai_auth/api_views.py
+++ ai_auth/api_views.py
@@ -1007,7 +1007,7 @@ def integrity_error(func):
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def TransactionSessionInfo(request):
-    session_id = request.POST.get('session_id')  
+    session_id = request.POST.get('session_id')
     if settings.STRIPE_LIVE_MODE == True :
         api_key = settings.STRIPE_LIVE_SECRET_KEY
     else:
@@ -1029,12 +1029,12 @@ def TransactionSessionInfo(request):
         charge = invoice.charge
         # if invoice == None:
 
-        #     return JsonResponse({"msg":"unable to find related data"},status=204,safe = False)          
+        #     return JsonResponse({"msg":"unable to find related data"},status=204,safe = False)
         pack = CreditPack.objects.get(product__prices__id=invoice.plan.id,type="Subscription")
         return JsonResponse({"email":charge.receipt_email,"purchased_plan":pack.name,"paid_date":charge.created,"currency":charge.currency,"amount":charge.amount,"plan_duration_start":invoice.subscription.current_period_start,"plan_duration_end":invoice.subscription.current_period_end,"paid":charge.paid,"payment_type":charge.payment_method.type,"txn_id":charge.balance_transaction_id},status=200,safe = False)
 
     elif response.mode == "payment":
-        try:    
+        try:
             charge = Charge.objects.get(payment_intent=response.payment_intent)
         except Charge.DoesNotExist:
              return JsonResponse({"msg":"unable to find related data"},status=204,safe = False)
@@ -1381,3 +1381,43 @@ def teams_list(request):
         except:
             print("No team")
     return JsonResponse({'My_external_team':teams})
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def TransactionSessionInfo(request):
+    session_id = request.POST.get('session_id')
+    if settings.STRIPE_LIVE_MODE == True :
+        api_key = settings.STRIPE_LIVE_SECRET_KEY
+    else:
+        api_key = settings.STRIPE_TEST_SECRET_KEY
+
+    stripe.api_key = api_key
+
+    response = stripe.checkout.Session.retrieve(
+                 session_id
+                    )
+    # print("Bank Details")
+    # print()
+    # print(response)
+    if response.mode == "subscription":
+        try:
+            invoice =Invoice.objects.get(subscription=response.subscription)
+        except Invoice.DoesNotExist:
+             return JsonResponse({"msg":"unable to find related data"},status=204,safe = False)
+        charge = invoice.charge
+        # if invoice == None:
+
+        #     return JsonResponse({"msg":"unable to find related data"},status=204,safe = False)
+        pack = CreditPack.objects.get(product__prices__id=invoice.plan.id,type="Subscription")
+        return JsonResponse({"email":charge.receipt_email,"purchased_plan":pack.name,"paid_date":charge.created,"amount":charge.amount,"plan_duration_start":invoice.subscription.current_period_start,"plan_duration_end":invoice.subscription.current_period_end,"paid":charge.paid,"payment_type":charge.payment_method.type,"txn_id":charge.balance_transaction_id},status=200,safe = False)
+
+    elif response.mode == "payment":
+        try:
+            charge = Charge.objects.get(payment_intent=response.payment_intent)
+        except Charge.DoesNotExist:
+             return JsonResponse({"msg":"unable to find related data"},status=204,safe = False)
+        pack = CreditPack.objects.get(product__prices__id=charge.metadata.get("price"),type="Addon")
+        return JsonResponse({"email":charge.receipt_email,"purchased_plan":pack.name,"paid_date":charge.created,"amount":charge.amount, "paid":charge.paid ,"payment_type":charge.payment_method.type, "txn_id":charge.balance_transaction_id},status=200,safe = False)
+    else:
+        return JsonResponse({"msg":"unable to find related data"},status=204,safe = False)
