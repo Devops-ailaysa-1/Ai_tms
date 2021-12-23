b44a6793e0e33c5d54efc7bad31cd8450fd6c46e
ThenuVijay <warunvijay@gmail.com>
2021-12-23 15:25:38 +0530

    project_list Api change

--- ai_workspace/api_views.py
+++ ai_workspace/api_views.py
@@ -1156,7 +1156,7 @@ def find_vendor(team,job):
 @api_view(['GET',])
 def project_list(request):
     proj_list=[]
-    queryset = Project.objects.filter(Q(project_jobs_set__job_tasks_set__assign_to = self.request.user)|Q(ai_user = self.request.user)|Q(team__owner = self.request.user)).distinct().order_by('-id')
+    queryset = Project.objects.filter(Q(project_jobs_set__job_tasks_set__assign_to = request.user)|Q(ai_user = request.user)|Q(team__owner = request.user)).distinct().order_by('-id')
     serializer = ProjectQuickSetupSerializer(queryset, many=True, context={'request': request})
     data = serializer.data
     for i in data:
