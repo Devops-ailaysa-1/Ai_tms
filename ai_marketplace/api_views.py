e0ba1d8ecd4a9a70a5f062c7734e94bc97351c45
ThenuVijay <warunvijay@gmail.com>
2022-01-05 18:26:35 +0530

    receiverlist check

--- ai_marketplace/api_views.py
+++ ai_marketplace/api_views.py
@@ -468,6 +468,7 @@ def get_available_threads(request):
             except:profile = None
             data = {'thread_id':i.id}
             chats = Notification.objects.filter(Q(data=data) & Q(verb='Message'))
+            print(chats)
             if chats:
                 count = request.user.notifications.filter(Q(data=data) & Q(verb='Message')).unread().count()
                 notification = chats.order_by('-timestamp').first()
