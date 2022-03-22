e5a96001426d74e6b9bfeadaa1e8b5f5d6997c5f
ThenuVijay <warunvijay@gmail.com>
2022-03-22 17:52:22 +0530

    sentry removed

--- ai_tms/settings.py
+++ ai_tms/settings.py
@@ -526,19 +526,19 @@ LOGGING = {
 
 
 # ERROR MONITORING USING SENTRY
-sentry_sdk.init(
-    dsn = os.getenv("dsn"),
-    integrations=[DjangoIntegration()],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate = os.getenv("traces_sample_rate"),
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii = os.getenv("send_default_pii")
-)
+# sentry_sdk.init(
+#     dsn = os.getenv("dsn"),
+#     integrations=[DjangoIntegration()],
+#
+#     # Set traces_sample_rate to 1.0 to capture 100%
+#     # of transactions for performance monitoring.
+#     # We recommend adjusting this value in production.
+#     traces_sample_rate = os.getenv("traces_sample_rate"),
+#
+#     # If you wish to associate users to errors (assuming you are using
+#     # django.contrib.auth) you may enable sending PII data.
+#     send_default_pii = os.getenv("send_default_pii")
+# )
 
 
 # FOR ERD DIAGRAM
