05073cd23b8854c2ce2a0ca848d6ae594f0d03bc
ThenuVijay <warunvijay@gmail.com>
2022-06-23 19:22:20 +0530

    Assignto serializer change

--- ai_workspace/serializers.py
+++ ai_workspace/serializers.py
@@ -894,8 +894,8 @@ class GetAssignToSerializer(serializers.Serializer):
 			tt=[]
 		request = self.context['request']
 		qs = obj.team.owner.user_info.filter(role=2) if obj.team else obj.user_info.filter(role=2)
-		qs = qs.filter(~Q(user__email = "ailaysateam@gmail.com"))
-		ser = HiredEditorDetailSerializer(qs,many=True,context={'request': request}).data
+		qs_ = qs.filter(~Q(hired_editor__email = "ailaysateam@gmail.com"))
+		ser = HiredEditorDetailSerializer(qs_,many=True,context={'request': request}).data
 		for i in ser:
 			if i.get("vendor_lang_pair")!=[]:
 				tt.append(i)
