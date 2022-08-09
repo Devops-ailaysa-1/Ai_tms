69aad81c6eaf0d28d8637fea8acd7d45ca08c19a
stephen raj <stephenrajrc@gmail.com>
2022-08-09 12:13:49 +0530

    fixed issue while generating PO

--- ai_tms/settings.py
+++ ai_tms/settings.py
@@ -496,56 +496,56 @@ SOCIALACCOUNT_AUTO_SIGNUP = True
 SOCIALACCOUNT_QUERY_EMAIL = True
 GOOGLE_CALLBACK_URL = os.getenv('GOOGLE_CALLBACK_URL')
 
-# LOGGING = {
-#     'version' : 1,
-#     'disable_existing_loggers' : False,
-#
-#     'formatters' : {
-#         'dev_formatter' : {
-#             'format' : '{levelname} {asctime} {pathname} {message}',
-#             'style' : '{',
-#         }
-#     },
-#
-#     'loggers' : {
-#         # 'django' : {
-#         #     'handlers' : ['file',],
-#         #     'level' : os.environ.get("LOGGING_LEVEL"), # to be received from .env file
-#         #     'propogate' : True,
-#         # },
-#
-#         'django' : {
-#             'handlers' : ['file_prod',],
-#             'level' : os.environ.get("LOGGING_LEVEL_PROD"), # to be received from .env file
-#             'propogate' : True,
-#         },
-#     },
-#
-#     'handlers' : {
-#
-#         'file' : {
-#             'level' : os.environ.get("LOGGING_LEVEL"), # to be received from .env file
-#             'class' : 'logging.FileHandler',
-#             'filename' : '{}.log'.format(os.environ.get("LOG_FILE_NAME")),  #filename to be received from .env
-#             'formatter' : 'dev_formatter',
-#         },
-#
-#        'file_prod' : {
-#             'level' : os.environ.get("LOGGING_LEVEL_PROD"), # to be received from .env file
-#             'class' : 'logging.FileHandler',
-#             'filename' : '{}.log'.format(os.environ.get("LOG_FILE_NAME_PROD")),  #filename to be received from .env
-#             'formatter' : 'dev_formatter',
-#         },
-#
-#         # 'mail_admins' : {
-#         #     'level' : 'ERROR',
-#         #     'class': 'django.utils.log.AdminEmailHandler',
-#         #     'formatter' : 'dev_formatter',
-#         # }
-#     },
-#
-#
-# }
+LOGGING = {
+    'version' : 1,
+    'disable_existing_loggers' : False,
+
+    'formatters' : {
+        'dev_formatter' : {
+            'format' : '{levelname} {asctime} {pathname} {message}',
+            'style' : '{',
+        }
+    },
+
+    'loggers' : {
+        # 'django' : {
+        #     'handlers' : ['file',],
+        #     'level' : os.environ.get("LOGGING_LEVEL"), # to be received from .env file
+        #     'propogate' : True,
+        # },
+
+        'django' : {
+            'handlers' : ['file_prod',],
+            'level' : os.environ.get("LOGGING_LEVEL_PROD"), # to be received from .env file
+            'propogate' : True,
+        },
+    },
+
+    'handlers' : {
+
+        'file' : {
+            'level' : os.environ.get("LOGGING_LEVEL"), # to be received from .env file
+            'class' : 'logging.FileHandler',
+            'filename' : '{}.log'.format(os.environ.get("LOG_FILE_NAME")),  #filename to be received from .env
+            'formatter' : 'dev_formatter',
+        },
+
+       'file_prod' : {
+            'level' : os.environ.get("LOGGING_LEVEL_PROD"), # to be received from .env file
+            'class' : 'logging.FileHandler',
+            'filename' : '{}.log'.format(os.environ.get("LOG_FILE_NAME_PROD")),  #filename to be received from .env
+            'formatter' : 'dev_formatter',
+        },
+
+        # 'mail_admins' : {
+        #     'level' : 'ERROR',
+        #     'class': 'django.utils.log.AdminEmailHandler',
+        #     'formatter' : 'dev_formatter',
+        # }
+    },
+
+
+}
 
 
 sentry_sdk.init(
