0eb743cb2df930177543690f70156a6e75c246ff
ThenuVijay <warunvijay@gmail.com>
2022-08-11 17:35:46 +0530

    mt engine added in file-jobs url

--- ai_workspace/api_views.py
+++ ai_workspace/api_views.py
@@ -453,7 +453,7 @@ class Files_Jobs_List(APIView):
         steps = ProjectStepsSerializer(steps,many=True)
         return Response({"files":files.data,"glossary_files":glossary_files.data,"glossary":glossary,"jobs": jobs.data, "subjects":subjects.data,\
                         "contents":contents.data, "steps":steps.data, "project_name": project.project_name, "team":project.get_team,\
-                         "team_edit":team_edit,"project_type_id":project.project_type.id,\
+                         "team_edit":team_edit,"project_type_id":project.project_type.id,"mt_engine_id":project.mt_engine_id,\
                          "project_deadline":project.project_deadline, "mt_enable": project.mt_enable, "revision_step_edit":project.PR_step_edit}, status=200)
 
 class TmxFilesOfProject(APIView):
