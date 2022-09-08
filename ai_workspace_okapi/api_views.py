85b6d6d30e6dc6d3e187f4a60512ecc75073a42a
ThenuVijay <warunvijay@gmail.com>
2022-09-08 17:24:15 +0530

    mt raw and tm view mt update change fix

--- ai_workspace_okapi/api_views.py
+++ ai_workspace_okapi/api_views.py
@@ -516,8 +516,9 @@ class MT_RawAndTM_View(views.APIView):
             if mt_raw:
                 ################################Update###########################
                 translation = get_translation(task_assign_mt_engine.id, mt_raw.segment.source, doc.source_language_code, doc.target_language_code)
-                tt = MT_RawTranslation.objects.filter(segment_id=segment_id).update(mt_raw = translation,mt_engine = task_assign_mt_engine)
-                return MT_RawSerializer(tt.first()).data, 200, "available"
+                MT_RawTranslation.objects.filter(segment_id=segment_id).update(mt_raw = translation,mt_engine = task_assign_mt_engine)
+                obj = MT_RawTranslation.objects.filter(segment_id=segment_id).first()
+                return MT_RawSerializer(obj).data, 200, "available"
             else:
                 ###############################Create############################
                 mt_raw_serlzr = MT_RawSerializer(data = {"segment": segment_id},\
