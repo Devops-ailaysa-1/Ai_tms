29d4b4cfbc1f603c963bda565688d9bda5e990b0
ThenuVijay <warunvijay@gmail.com>
2022-09-21 17:03:21 +0530

    bug fix in transcribe

--- ai_workspace/api_views.py
+++ ai_workspace/api_views.py
@@ -1825,8 +1825,8 @@ def transcribe_short_file(speech_file,source_code,obj,length,user):
         print(u"Transcript: {}".format(result.alternatives[0].transcript))
         transcript += result.alternatives[0].transcript
         file_length = int(result.result_end_time.seconds)
-    print("Len--------->",file_length)
-    ser = TaskTranscriptDetailSerializer(data={"transcripted_text":transcript,"task":obj.id,"audio_file_length":file_length,"user":user.id})
+        print("Len--------->",file_length)
+    ser = TaskTranscriptDetailSerializer(data={"transcripted_text":transcript,"task":obj.id,"audio_file_length":length,"user":user.id})
     if ser.is_valid():
         ser.save()
         return (ser.data)
@@ -1878,12 +1878,12 @@ def transcribe_long_file(speech_file,source_code,filename,obj,length,user):
     for result in response.results:
         transcript += result.alternatives[0].transcript
         file_length = int(result.result_end_time.seconds)
-    print("Len------->",file_length)
+        print("Len------->",file_length)
     print("Transcript--------->",transcript)
 
     delete_blob(bucket_name, destination_blob_name)
 
-    ser = TaskTranscriptDetailSerializer(data={"transcripted_text":transcript,"task":obj.id,"audio_file_length":file_length,"user":user.id})
+    ser = TaskTranscriptDetailSerializer(data={"transcripted_text":transcript,"task":obj.id,"audio_file_length":length,"user":user.id})
     if ser.is_valid():
         ser.save()
         return (ser.data)
