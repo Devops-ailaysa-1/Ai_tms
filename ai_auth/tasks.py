288cef2d265d327ff03d11c84b556f747396ed14
ThenuVijay <warunvijay@gmail.com>
2022-10-11 18:10:10 +0530

    google_long_text_file_process_cel_task change

--- ai_auth/tasks.py
+++ ai_auth/tasks.py
@@ -387,7 +387,7 @@ def google_long_text_file_process_cel(consumable_credits,document_user_id,file_p
     from ai_workspace_okapi.api_views import long_text_process
     document_user = AiUser.objects.get(id = document_user_id)
     obj = Task.objects.get(id=task_id)
-    MTonlytaskCeleryStatus.objects.create(task_id=task.id,status=1,task_name='google_long_text_file_process_cel',celery_task_id=google_long_text_file_process_cel.request.id)
+    MTonlytaskCeleryStatus.objects.create(task_id=obj.id,status=1,task_name='google_long_text_file_process_cel',celery_task_id=google_long_text_file_process_cel.request.id)
     tr = long_text_process(consumable_credits,document_user,file_path,obj,target_language,voice_gender,voice_name)
     #MTonlytaskCeleryStatus.objects.create(task_id=task.id,status=2,task_name='google_long_text_file_process_cel',celery_task_id=google_long_text_file_process_cel.request.id)
     logger.info("Text to speech document called")
