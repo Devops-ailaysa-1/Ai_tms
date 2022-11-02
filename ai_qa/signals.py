c0e5eaf92a8ff52b757906d08918020087bd9efe
ThenuVijay <warunvijay@gmail.com>
2022-11-02 17:31:20 +0530

    Untranslatables and Forbitten words included

---
+++ ai_qa/signals.py
@@ -0,0 +1,33 @@
+from ai_qa import models as qa_model
+from tablib import Dataset
+
+def update_words_from_forbidden_file(sender, instance, *args, **kwargs):
+    s = open(instance.forbidden_file.path)
+    text = s.readlines()
+    for i in text:
+        qa_model.ForbiddenWords.objects.create(words=i.strip(),job=instance.job,project=instance.project,file=instance)
+    print("ForbiddenWords Added")
+
+
+def delete_words_from_ForbiddenWords(sender, instance, *args, **kwargs):
+    try:
+        forbidden_words = qa_model.ForbiddenWords.objects.filter(file_id = instance.id)
+        forbidden_words.delete()
+    except:
+        pass
+    print("ForbiddenWords Deleted")
+
+def update_words_from_untranslatable_file(sender, instance, *args, **kwargs):
+    s = open(instance.untranslatable_file.path)
+    text = s.readlines()
+    for i in text:
+        qa_model.UntranslatableWords.objects.create(words=i.strip(),job=instance.job,project=instance.project,file=instance)
+    print("Untranslatables added")
+
+def delete_words_from_Untranslatable(sender, instance, *args, **kwargs):
+    try:
+        untranslatable_words = qa_model.UntranslatableWords.objects.filter(file_id = instance.id)
+        untranslatable_words.delete()
+    except:
+        pass
+    print("UntranslatableWords Deleted")
