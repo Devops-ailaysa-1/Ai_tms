0b5088b69be47fe81f2d301b7484a99260608d84
ThenuVijay <warunvijay@gmail.com>
2022-12-27 15:57:38 +0530

    AiWriter prompt serializer update

--- ai_exportpdf/views.py
+++ ai_exportpdf/views.py
@@ -219,7 +219,8 @@ class AiPromptViewset(viewsets.ViewSet):
     def create(self,request):
         # keywords = request.POST.getlist('keywords')
         targets = request.POST.getlist('get_result_in')
-        serializer = AiPromptSerializer(data={**request.POST.dict(),'user':self.request.user.id,'targets':targets})
+        char_limit = request.POST.get('response_charecter_limit',256)
+        serializer = AiPromptSerializer(data={**request.POST.dict(),'user':self.request.user.id,'targets':targets,'response_charecter_limit':char_limit})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -236,16 +237,6 @@ class AiPromptResultViewset(viewsets.ViewSet):
         serializer = AiPromptGetSerializer(prmp_obj)
         return Response(serializer.data)
 
-        # query_set = prmp_obj.ai_prompt.all()
-        # serializer = AiPromptResultSerializer(query_set ,many =True)
-        # return Response(serializer.data)
-
-    # def create(self,request):
-    #     serializer = AiPromptResultSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors)
 
     
 
@@ -261,23 +252,35 @@ def customize_text_openai(request):
     if lang!= 'en':
         user_text_mt_en = get_translation(mt_engine_id=1 , source_string = user_text,
                                        source_lang_code=lang , target_lang_code='en')
-        # user_text = customize +" this: "+ user_text_mt_en
-        # print("user_text " ,user_text)
-        # response = get_prompt(user_text ,model_name=openai_model , max_token =100,n=1 )
         response = get_prompt_edit(input_text=user_text ,instruction=customize)
         txt_generated = response['choices'][0]['text']
         user_text = get_translation(mt_engine_id=1 , source_string = txt_generated,
                                        source_lang_code='en' , target_lang_code=lang)
         
     else:##english
-        # user_text = customize +" text below\n:"+ user_text
-        # user_text = customize +" this:'{}'".format(user_text)
-        # print("user_text " ,user_text)
-        # response = get_prompt(user_text ,model_name=openai_model ,max_token =200 ,n=1 )
         response = get_prompt_edit(input_text=user_text ,instruction=customize)
         user_text = response['choices'][0]['text']
     total_tokens = response['usage']['total_tokens']
     return Response({'customize_text': user_text ,'lang':lang ,'customize_cat':customize},status=200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     # consumable_credits = get_consumable_credits_for_openai_text_generator(total_token =tot_tokn )
     # if initial_credit > consumable_credits:
