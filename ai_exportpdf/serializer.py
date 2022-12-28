a1eb7cb3a30f30e584aaa13a6b466ae38973ca4a
ThenuVijay <warunvijay@gmail.com>
2022-12-28 18:08:42 +0530

    cuztomize credit deduction added

--- ai_exportpdf/serializer.py
+++ ai_exportpdf/serializer.py
@@ -172,7 +172,7 @@ class AiPromptGetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AiPrompt
-        fields = ('user','prompt_string','source_prompt_lang','description','catagories','sub_catagories','Tone',
+        fields = ('id','user','prompt_string','source_prompt_lang','description','catagories','sub_catagories','Tone',
                     'product_name','keywords','created_at','prompt_results',)#,'ai_prompt'
         
 
