fdea09babcb7965707ee0e228f99b2381a5043fb
ThenuVijay <warunvijay@gmail.com>
2022-12-30 13:42:21 +0530

    get_prompt change

--- ai_exportpdf/utils.py
+++ ai_exportpdf/utils.py
@@ -296,7 +296,7 @@ import openai
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
-def openai_endpoint(prompt,max_token=256,
+def openai_endpoint(prompt,max_token=30,#256
                     temperature=0.7,frequency_penalty=1,
                     presence_penalty=1,top_p=1):
  
@@ -392,7 +392,7 @@ def openai_text_trim(text):
 
 
 def get_prompt(prompt ,model_name , max_token ,n ):
-    max_token = 256
+    #max_token = 256
     temperature=0.7
     frequency_penalty = 1
     presence_penalty = 1
@@ -401,7 +401,7 @@ def get_prompt(prompt ,model_name , max_token ,n ):
                 model=model_name, 
                 prompt=prompt.strip(),
                 temperature=temperature,
-                max_tokens=max_token,
+                max_tokens=int(max_token),
                 top_p=top_p,
                 frequency_penalty=frequency_penalty,
                 presence_penalty=presence_penalty,
