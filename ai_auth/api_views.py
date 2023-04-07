59f011cb190d4aec3d95c2fa849be0d859929981
ThenuVijay <76106957+ThenuVijay@users.noreply.github.com>
2023-04-07 16:49:53 +0530

    Update api_views.py

--- ai_auth/api_views.py
+++ ai_auth/api_views.py
@@ -1973,7 +1973,7 @@ def vendor_onboard_complete(request):#######while using social signups##########
 
 
 import quickemailverification
-API_KEY = os.getenv('API_KEY')
+API_KEY = os.getenv('EMAIL_VERIFY_API_KEY')
 client = quickemailverification.Client(API_KEY)
 
 @api_view(['POST'])
