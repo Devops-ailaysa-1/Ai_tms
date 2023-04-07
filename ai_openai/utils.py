f2372f50d31b7a8c139f3ae29460147d79d2fc48
ThenuVijay <76106957+ThenuVijay@users.noreply.github.com>
2023-04-07 17:19:52 +0530

    Update utils.py

--- ai_openai/utils.py
+++ ai_openai/utils.py
@@ -106,3 +106,14 @@ def get_img_content_from_openai_url(image_url):
     pil_img.save(img_byte_arr, format='PNG')
     img_byte_arr = img_byte_arr.getvalue()
     return img_byte_arr
+
+
+@backoff.on_exception(backoff.expo, (openai.error.RateLimitError,openai.error.APIConnectionError,) , max_time=30)
+def get_prompt_chatgpt_turbo(prompt,n):
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": prompt}
+    ],n=n
+    )
+    return completion
