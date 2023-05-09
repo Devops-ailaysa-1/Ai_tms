95bd6e9b2829dd6a6336625ea52ba3073dc804c7
Hemanthkumar2112 <hemanthmurugan21@gmail.com>
2023-05-09 15:36:59 +0530

    stop_task_remove

--- ai_imagetranslation/utils.py
+++ ai_imagetranslation/utils.py
@@ -174,8 +174,7 @@ def inpaint_image_creation(image_details):
         output=inpaint_image(img_path, mask_path)
         if output['code']==200:
             if output['result'].shape[0]==np.prod(img.shape):
-                res=np.reshape(output['result'],img.shape)   
-
+                res=np.reshape(output['result'],img.shape)  
                 diff=cv2.absdiff(img,res)
                 diff=lama_diff(mask,diff)
                 diff=cv2.cvtColor(diff, cv2.COLOR_BGR2RGB)
@@ -187,8 +186,6 @@ def inpaint_image_creation(image_details):
                 dst=np.asarray(dst)
                 dst_final=np.copy(dst)
                 dst_final=cv2.cvtColor(dst_final,cv2.COLOR_BGR2RGB)
-                # ray_img=cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-                # dst=cv2.inpaint(img,ray_img,3,cv2.INPAINT_TELEA)
                 return dst_final,image_text_details
             else:return serializers.ValidationError({'shape_error':'pred_output_shape is dissimilar to user_image'})
                 
