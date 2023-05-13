8fa2083b0af2c3e24bf55f1342b9439097b7b9cf
Hemanthkumar2112 <hemanthmurugan21@gmail.com>
2023-05-13 10:28:49 +0530

    vendor lang pair

--- ai_vendor/admin.py
+++ ai_vendor/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import VendorsInfo
+from .models import VendorsInfo,VendorLanguagePair,VendorServiceInfo,VendorServiceTypes
 
 admin.site.register(VendorsInfo)
+
+
+admin.site.register(VendorLanguagePair)
+admin.site.register(VendorServiceInfo)
+admin.site.register(VendorServiceTypes)
