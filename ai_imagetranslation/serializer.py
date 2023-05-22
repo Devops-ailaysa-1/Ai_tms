ba8cb4b59f3e5978a09882eeaaa41ec199f02ac8
Hemanthkumar2112 <hemanthmurugan21@gmail.com>
2023-05-22 17:15:56 +0530

    json_project_id

--- ai_imagetranslation/serializer.py
+++ ai_imagetranslation/serializer.py
@@ -143,7 +143,7 @@ class ImageTranslateSerializer(serializers.ModelSerializer):
                 basic_json_copy['objects']=[img_json_copy]+text_box_list
                 basic_json_copy['backgroundImage']['width']=int(instance.width)
                 basic_json_copy['backgroundImage']['height']=int(instance.height)
-                basic_json_copy['projectid']={'langId':src_lang.locale.first().id,'langNo':src_lang.id ,'projId':instance.id,'projectType':'image-translate'}
+                basic_json_copy['projectid']={'langId':None,'langNo':src_lang.id ,'projId':instance.id,'projectType':'image-translate'}
                 # print("basic_json_copy",basic_json_copy)
                 # print("img_json_copy",img_json_copy)
                 # print("text_box_list",text_box_list)
