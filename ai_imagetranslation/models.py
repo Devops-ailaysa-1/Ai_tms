49c79c6ad610d3a6b80d64fc328ae8a8e3d1fe46
Hemanthkumar2112 <hemanthmurugan21@gmail.com>
2023-05-23 12:09:00 +0530

    target_json_database_create

--- ai_imagetranslation/models.py
+++ ai_imagetranslation/models.py
@@ -18,35 +18,34 @@ def user_directory_path_image_translate_result(instance, filename):
 
 
 class Imageload(models.Model):
-    user = models.ForeignKey(AiUser, on_delete=models.CASCADE)
-    image = models.FileField(upload_to=user_directory_path_image_load, blank=True ,null=True)
-    file_name = models.CharField(max_length=200 , blank=True,null=True)
-    types = models.CharField(max_length=10 , blank=True,null=True)
-    height = models.CharField(max_length=10,blank=True,null=True)
-    width = models.CharField(max_length=10,blank=True,null=True)
-    created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
-    updated_at= models .DateTimeField(auto_now=True,null=True,blank=True)
+    user=models.ForeignKey(AiUser,on_delete=models.CASCADE)
+    image=models.FileField(upload_to=user_directory_path_image_load,blank=True ,null=True)
+    file_name=models.CharField(max_length=200,blank=True,null=True)
+    types=models.CharField(max_length=10,blank=True,null=True)
+    height=models.CharField(max_length=10,blank=True,null=True)
+    width=models.CharField(max_length=10,blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    updated_at=models.DateTimeField(auto_now=True,null=True,blank=True)
     
     
 class ImageTranslate(models.Model):
     user=models.ForeignKey(AiUser,on_delete=models.CASCADE)
     image=models.FileField(upload_to=user_directory_path_image_translate_image,blank=True,null=True)
-    project_name=models.CharField(max_length=200 , blank=True,null=True)
+    project_name=models.CharField(max_length=200,blank=True,null=True)
     types=models.CharField(max_length=10,blank=True,null=True)
     height=models.CharField(max_length=10,blank=True,null=True)
     width=models.CharField(max_length=10,blank=True,null=True)
     mask=models.FileField(upload_to=user_directory_path_image_translate_process,blank=True,null=True)
     mask_json=models.JSONField(blank=True,null=True)
     inpaint_image=models.FileField(upload_to=user_directory_path_image_translate_result,blank=True,null=True)
-    create_inpaint_pixel_location=models.FileField(upload_to =user_directory_path_image_translate_process,blank=True,null=True)
+    create_inpaint_pixel_location=models.FileField(upload_to=user_directory_path_image_translate_process,blank=True,null=True)
     source_canvas_json=models.JSONField(blank=True,null=True)
     source_bounding_box=models.JSONField(blank=True,null=True)
     source_language=models.ForeignKey(to=LanguagesLocale,on_delete=models.CASCADE,blank=True,null=True, related_name='s_lang')
     created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
     updated_at= models.DateTimeField(auto_now=True,null=True,blank=True)
  
- 
-    
+
 def user_directory_path_image_translate_thumbnail(instance, filename):
     return '{0}/{1}/{2}'.format(instance.source_image.user.uid,"image_translate/thumbnail",filename) 
 
@@ -55,16 +54,31 @@ def user_directory_path_image_translate_export(instance, filename):
     return '{0}/{1}/{2}'.format(instance.source_image.user.uid, "image_translate/export",filename) 
 
 class ImageInpaintCreation(models.Model):
-    source_image=models.ForeignKey(to=ImageTranslate, blank=True,null=True,on_delete=models.CASCADE,related_name='s_im')
+    source_image=models.ForeignKey(to=ImageTranslate,blank=True,null=True,on_delete=models.CASCADE,related_name='s_im')
     target_language=models.ForeignKey(to=LanguagesLocale,on_delete=models.CASCADE,related_name='t_lang')
-    target_canvas_json=models.JSONField(blank=True,null=True)
-    target_bounding_box=models.JSONField(blank=True,null = True)
-    thumbnail=models.FileField(upload_to=user_directory_path_image_translate_thumbnail,blank = True , null = True )
-    export=models.FileField(upload_to=user_directory_path_image_translate_export,blank = True , null = True ) 
+    target_canvas_json=models.JSONField(blank=True,null=True) ############
+    target_bounding_box=models.JSONField(blank=True,null=True)  ################
+    thumbnail=models.FileField(upload_to=user_directory_path_image_translate_thumbnail,blank=True,null=True) ############
+    export=models.FileField(upload_to=user_directory_path_image_translate_export,blank=True,null=True)  ##################
     created_at=models.DateTimeField(auto_now_add=True,blank=True,null=True)
     updated_at=models.DateTimeField(auto_now=True,null=True,blank=True)
 
     class Meta:
         ordering = ['id']
+
+def user_directory_path_inpaint_image(instance, filename):
+    return '{0}/{1}/{2}'.format(instance.inpaint_create.source_image.user.uid, "image_translate/inpaint",filename) 
+
+class TargetInpaintimage(models.Model):
+    inpaint_create=models.ForeignKey(to=ImageInpaintCreation,blank=True,null=True,on_delete=models.CASCADE,related_name='tar_im_create')
+    mask_json=models.JSONField(blank=True,null=True)
+    inpaint_image=models.FileField(upload_to=user_directory_path_inpaint_image,blank=True,null=True)
+    target_canvas_json=models.JSONField(blank=True,null=True)
+    thumbnail=models.FileField(upload_to=user_directory_path_image_translate_thumbnail,blank=True,null=True)
+    export=models.FileField(upload_to=user_directory_path_image_translate_export,blank=True,null=True) 
+
+    
+
+
 
 
--- ai_imagetranslation/serializer.py
+++ ai_imagetranslation/serializer.py
@@ -1,4 +1,4 @@
-from ai_imagetranslation.models import (Imageload,ImageInpaintCreation,ImageTranslate)
+from ai_imagetranslation.models import (Imageload,ImageInpaintCreation,ImageTranslate,TargetInpaintimage)
 from ai_staff.models import Languages
 from rest_framework import serializers
 from PIL import Image
@@ -31,11 +31,19 @@ class ImageloadSerializer(serializers.ModelSerializer):
         return instance
 
 from ai_canvas.template_json import img_json,basic_json
-class ImageInpaintCreationSerializer(serializers.ModelSerializer):
-    
+
+class TargetInpaintimageSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = ImageInpaintCreation
         fields = "__all__"
+
+class ImageInpaintCreationSerializer(serializers.ModelSerializer):
+    tar_im_create=TargetInpaintimageSerializer(many=True,read_only=True)
+    class Meta:
+        model = ImageInpaintCreation
+        fields = ("id",'source_image','target_language','target_canvas_json','target_bounding_box',
+                    'export','thumbnail','created_at','updated_at','tar_im_create')
         
     def to_representation(self, instance):
         representation = super().to_representation(instance)
@@ -150,7 +158,9 @@ class ImageTranslateSerializer(serializers.ModelSerializer):
             ####to create instance for target language
             for tar_lang in inpaint_creation_target_lang:
                 tar_bbox=ImageInpaintCreation.objects.create(source_image=instance,target_language=tar_lang.locale.first())   
-                # source_bbox=source_bounding_box               
+                tar_inpaint_img=TargetInpaintimage.objects.create(inpaint_create=tar_bbox,
+                                                                  mask_json=instance.mask_json,inpaint_image=instance.inpaint_image,
+                                                                  )
                 tar_json_copy=copy.deepcopy(instance.source_canvas_json)
                 tar_json_copy['projectid']={'langId':tar_bbox.id,'langNo':src_lang.id ,
                                             'projId':instance.id,'projectType':'image-translate'}
@@ -160,14 +170,10 @@ class ImageTranslateSerializer(serializers.ModelSerializer):
                         translate_bbox=get_translation(1,source_string=i['text'],source_lang_code='en',
                                                      target_lang_code=tar_lang.locale.first().locale_code)
                         i['text']=translate_bbox
-
-                # for text in source_bbox.values(): 
-                #     translate_bbox=get_translation(1,source_string=text['text'],source_lang_code='en',
-                #                                      target_lang_code=tar_lang.locale.first().locale_code)
-                    # text['text']=translate_bbox
-                # tar_bbox.target_bounding_box=source_bbox
                 tar_bbox.target_canvas_json=tar_json_copy
+                tar_inpaint_img.target_canvas_json=tar_json_copy
                 tar_bbox.save()
+                tar_inpaint_img.save()
             instance.save()
             return instance
  
