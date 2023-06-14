b07a7270a4a663d431601f0cb8537541ffe9ea3d
Hemanthkumar2112 <hemanthmurugan21@gmail.com>
2023-06-14 16:39:58 +0530

    canvas

--- ai_canvas/utils.py
+++ ai_canvas/utils.py
@@ -122,13 +122,13 @@ def canvas_translate_json_fn(canvas_json,src_lang,languages):
             for count , i in enumerate(canvas_json_copy['objects']):
                 if i['type']== 'textbox':
                     text = i['text'] 
-                    fontSize=canvas_json_copy['template_json']['objects'][count]['fontSize']
+                    fontSize=canvas_json_copy['objects'][count]['fontSize']
                     tar_word=get_translation(1,source_string = text,source_lang_code=src_lang,target_lang_code = lang.strip())
                     canvas_json_copy['objects'][count]['text'] =  tar_word
                     
                     text_width, text_height=text_size(text,fontSize)
                     font_size=calculate_font_size(text_width, text_height,tar_word,fontSize)
-                    canvas_json_copy['fontSize']=font_size
+                    canvas_json_copy['objects'][count]['fontSize']=font_size
                     # fontSize=calculate_font_size(box_width=width, box_height=height,text=tar_word,font_size=fontSize)
                     # canvas_json_copy['fontSize']=fontSize
                     if i['type'] == 'group':
