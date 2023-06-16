cbb839b41a7939c8ba6dc5e01cec3340307cd0f3
Hemanthkumar2112 <hemanthmurugan21@gmail.com>
2023-06-16 11:26:43 +0530

    create shape

--- ai_staff/urls.py
+++ ai_staff/urls.py
@@ -74,7 +74,7 @@ urlpatterns += [
      #path('insert',views.Bulk_insert)
      path('social-media-size/', api_views.SocialMediaSizeViewset_ser.as_view({'get': 'list'}), name='socialmediasize'),
      path('image-gen-resolution/',api_views.ImageGeneratorResolutionViewset.as_view({'get':'list'})),
-      # path('design-shape/',api_views.DesignShapeViewset.as_view({'get':'list'})),
+#    path('design-shape/',api_views.DesignShapeViewset.as_view({'get':'list'})),
 ]
 
  
