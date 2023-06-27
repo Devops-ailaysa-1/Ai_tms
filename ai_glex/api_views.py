23127652ef2b52d1aadd8e3eb22c89487bcf15aa
ThenuVijay <warunvijay@gmail.com>
2023-06-27 22:24:03 +0530

    glex,project analysis download,self_asset in mt_raw are commented for now. needs to be tested

--- ai_glex/api_views.py
+++ ai_glex/api_views.py
@@ -141,8 +141,8 @@ class GlossaryFileView(viewsets.ViewSet):
         if serializer.is_valid():
             print(serializer.is_valid())
             serializer.save()
-            file_ids = [i.get('id') for i in serializer.data]
-            update_words_from_template.apply_async((file_ids,))
+            # file_ids = [i.get('id') for i in serializer.data]
+            # update_words_from_template.apply_async((file_ids,))
             return Response(serializer.data, status=201)
         else:
             return Response (serializer.errors,status=400)
