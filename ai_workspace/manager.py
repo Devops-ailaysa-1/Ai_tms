e6e499b104f8ed226e60f92e111d539786a0b8bd
ThenuVijay <warunvijay@gmail.com>
2023-07-07 13:13:13 +0530

    default choice list create while project_creation

--- ai_workspace/manager.py
+++ ai_workspace/manager.py
@@ -78,10 +78,12 @@ class ProjectManager(models.Manager):
             from ai_workspace_okapi.models import ChoiceLists,ChoiceListSelected
             jobs = project.project_jobs_set.all()
             target_languages = [i.target_language_id for i in jobs]
-            query = ChoiceLists.objects.filter(user = project.ai_user).filter(language_id__in = target_languages).filter(is_default=True)
+            query = [ChoiceLists.objects.get_or_create(user=project.ai_user,language_id=i,is_default=True) for i in target_languages]
+            #query = ChoiceLists.objects.filter(user = project.ai_user).filter(language_id__in = target_languages).filter(is_default=True)
             print("Qr------------>",query)
-            if query:
-                ch = [ChoiceListSelected.objects.get_or_create(project=project,choice_list=i) for i in query]
+            objects = [item[0] for item in query]
+            if objects:
+                ch = [ChoiceListSelected.objects.get_or_create(project=project,choice_list=i) for i in objects]
                 print("Ch--------->",ch)
             return None
 
