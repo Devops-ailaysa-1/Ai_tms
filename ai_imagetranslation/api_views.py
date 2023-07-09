10349d562e4da7e2d3fc5f9ce309674cfc336c9d
Hemanthkumar2112 <hemanthmurugan21@gmail.com>
2023-07-09 20:45:46 +0530

    bug in canvas design

--- ai_imagetranslation/api_views.py
+++ ai_imagetranslation/api_views.py
@@ -65,8 +65,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from django.http import JsonResponse
 class ImageTranslateViewset(viewsets.ViewSet,PageNumberPagination):
     permission_classes = [IsAuthenticated,]
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ('project_name','types')
+    ffilter_backends = (filters.SearchFilter,)
     search_fields = ['project_name','types']
     page_size=20
     def get_object(self, pk):
