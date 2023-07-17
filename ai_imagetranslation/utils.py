073c555262c0378cfdc86fa64a1aed45d56c36ad
unknown <Hemanthkumar2112>
2023-07-17 14:08:02 +0530

    ocr bug

--- ai_imagetranslation/utils.py
+++ ai_imagetranslation/utils.py
@@ -60,20 +60,20 @@ def creating_image_bounding_box(image_path,color_find_image_diff):
     for i in  texts.pages:
         # x,y,w,h=i.bounding_box.vertices[0].x ,i.bounding_box.vertices[1].y,i.bounding_box.vertices[2].x,i.bounding_box.vertices[3].y 
         for j in i.blocks:
+            count=0
+            text_uuid=uuid.uuid4()
+            textbox_=copy.deepcopy(textbox_json)
+            name="Textbox_"+(str(text_uuid))
+            textbox_['id']="text_"+(str(text_uuid))
+            count+=1
+            textbox_['name']=name
+            x,y,w,h=j.bounding_box.vertices[0].x ,j.bounding_box.vertices[1].y,j.bounding_box.vertices[2].x,j.bounding_box.vertices[3].y 
+            textbox_['left']=x
+            textbox_['top']=y
+            textbox_['width']=w-x
+            textbox_['height']=h
+            final_color=color_extract_from_text(x,y,w,h,pillow_image_to_extract_color)
             for k in j.paragraphs:
-                count=0
-                text_uuid=uuid.uuid4()
-                textbox_=copy.deepcopy(textbox_json)
-                name="Textbox_"+(str(text_uuid))
-                textbox_['id']="text_"+(str(text_uuid))
-                count+=1
-                textbox_['name']=name
-                x,y,w,h=j.bounding_box.vertices[0].x ,j.bounding_box.vertices[1].y,j.bounding_box.vertices[2].x,j.bounding_box.vertices[3].y 
-                textbox_['left']=x
-                textbox_['top']=y
-                textbox_['width']=w-x
-                textbox_['height']=h
-                final_color=color_extract_from_text(x,y,w,h,pillow_image_to_extract_color)
                 for a in k.words:
                     text_list.append(" ") 
                     font_size=[]
@@ -83,18 +83,18 @@ def creating_image_bounding_box(image_path,color_find_image_diff):
                         fx,fy,fw,fh=b.bounding_box.vertices[0].x,b.bounding_box.vertices[1].y,b.bounding_box.vertices[2].x,b.bounding_box.vertices[3].y
                         font_size.append(fh-fy)  
                         font_size2.append(fw-fx)
-                text_and_bounding_results[no_of_segments]={"text":"".join(text_list),"bbox":[x,y,w,h],"fontsize":sum(font_size)//len(font_size),
-                                                        "fontsize2":sum(font_size2)//len(font_size2),"color1":final_color}
-                                                        # "poly_line":poly_line}
-                textbox_['text']="".join(text_list).strip()
-                print("text-------------->>","".join(text_list))
-                textbox_['fill']="rgb{}".format(tuple(final_color[0]))
-                # textbox_['angle']=arrival_angle
-                font=max([sum(font_size)//len(font_size),sum(font_size2)//len(font_size2)])+5
-                textbox_['fontSize']=font
-                no_of_segments+=1
-                text_list=[]
-                text_box_list.append(textbox_)
+                    text_and_bounding_results[no_of_segments]={"text":"".join(text_list),"bbox":[x,y,w,h],"fontsize":sum(font_size)//len(font_size),
+                                                            "fontsize2":sum(font_size2)//len(font_size2),"color1":final_color}
+                                                            # "poly_line":poly_line}
+                    textbox_['text']="".join(text_list).strip()
+                    print("text-------------->>","".join(text_list))
+                    textbox_['fill']="rgb{}".format(tuple(final_color[0]))
+                    # textbox_['angle']=arrival_angle
+                    font=max([sum(font_size)//len(font_size),sum(font_size2)//len(font_size2)])+5
+                    textbox_['fontSize']=font
+                    no_of_segments+=1
+                    text_list=[]
+                    text_box_list.append(textbox_)
     return text_and_bounding_results,text_box_list
  
 
