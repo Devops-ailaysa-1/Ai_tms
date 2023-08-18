59c7e55bb90a58c8a219b69e8d8250393d0eb0a5
Hemanthkumar2112 <hemanthmurugan21@gmail.com>
2023-08-18 10:13:54 +0530

    test

--- ai_imagetranslation/admin.py
+++ ai_imagetranslation/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from ai_imagetranslation.models import CustomImageGenerationStyle ,ImageStyleCategories,ImageModificationTechnique
+from ai_imagetranslation.models import CustomImageGenerationStyle ,ImageStyleCategories,ImageModificationTechnique,GeneralPromptList
 admin.site.register(CustomImageGenerationStyle)
 admin.site.register(ImageStyleCategories)
 admin.site.register(ImageModificationTechnique)
+admin.site.register(GeneralPromptList)
+
+
