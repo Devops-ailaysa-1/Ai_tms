c4359a56e4bacd75e9316dd3829a48c488bafe05
azar <azarutheen42@gmail.com>
2023-09-07 12:59:42 +0530

    color updated

--- ai_canvas/api_views.py
+++ ai_canvas/api_views.py
@@ -1611,7 +1611,7 @@ def genarate_template(limit,template_data,prompt_id,img_instance,template,font_f
             elif  obj["type"] =="group":
                  for k in obj["objects"]:
                       if k["type"]=="textbox":
-                        k["fill"]=color_attr["textbox"]
+                        k["fill"]=color_attr["grouppathtext"]
                         # data["objects"].append(k)
                         # obj["objects"].remove(k)
                       else:
