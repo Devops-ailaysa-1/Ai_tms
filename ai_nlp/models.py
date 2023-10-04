d273e461525817dd41e025f6c90b24f4ab933a66
Hemanthkumar2112 <hemanthmurugan21@gmail.com>
2023-10-04 12:40:09 +0530

    celery added

--- ai_nlp/models.py
+++ ai_nlp/models.py
@@ -12,7 +12,7 @@ def user_directory_path_pdf_thumbnail(instance, filename):
 
 class PdffileUpload(models.Model):
     user = models.ForeignKey(AiUser,on_delete=models.CASCADE)
-    file = models.FileField(upload_to=user_directory_path_pdf_upload,null=False,blank=True)
+    file = models.FileField(upload_to=user_directory_path_pdf_upload,null=True,blank=True)
     file_name = models.CharField(max_length=200,null=True,blank=True)
     pdf_thumbnail = models.FileField(upload_to=user_directory_path_pdf_thumbnail,blank=True ,null=True)
     vector_embedding_path = models.CharField(max_length=200,null=True,blank=True)
@@ -21,7 +21,9 @@ class PdffileUpload(models.Model):
     created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
     created_by = models.ForeignKey(AiUser,null=True, blank=True, on_delete=models.SET_NULL,related_name='pdfchat_created_by')
     updated_at= models.DateTimeField(auto_now=True,null=True,blank=True)
-
+    celery_id = models.CharField(max_length=200,null=True,blank=True)
+    status = models.CharField(max_length=200,null=True,blank=True)
+    website = models.TextField(null=True,blank=True)
 
 class PdffileChatHistory(models.Model):
     pdf_file =models.ForeignKey(PdffileUpload,on_delete=models.CASCADE,related_name="pdf_file_chat")
@@ -29,3 +31,11 @@ class PdffileChatHistory(models.Model):
     answer = models.CharField(max_length=2000,null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
     updated_at= models.DateTimeField(auto_now=True,null=True,blank=True)
+
+# class ChatTokenUsage(models.Model):
+#     chat_his = models.OneToOneField(PdffileUpload,on_delete=models.CASCADE,related_name="pdf_file_chat")
+#     created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+#     updated_at= models.DateTimeField(auto_now=True,null=True,blank=True)
+#     prompt_token=models.IntegerField(null=True,blank=True)
+#     complete_token=models.IntegerField(null=True,blank=True)
+#     tot_token=models.IntegerField(null=True,blank=True)
