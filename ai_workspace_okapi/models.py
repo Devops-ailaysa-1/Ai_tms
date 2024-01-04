9ce4bea79425f9c964b3f60cc18ba5f5b611f260
ThenuVijay <warunvijay@gmail.com>
2024-01-04 15:04:36 +0530

    Merge target segments

--- ai_workspace_okapi/models.py
+++ ai_workspace_okapi/models.py
@@ -260,9 +260,9 @@ class MergeSegment(BaseSegment):
 
     def update_segments(self, segs):
         self.source = "".join([seg.source for seg in segs])
-        self.target = ""
+        self.target = "".join([seg.target for seg in segs if seg.target])
         self.coded_source = "".join([seg.coded_source for seg in segs])
-        self.temp_target = ""
+        self.temp_target = "".join([seg.temp_target for seg in segs if seg.temp_target])
         self.target_tags = "".join([seg.target_tags for seg in segs])
         self.tagged_source = "".join([seg.tagged_source for seg in segs])
         self.coded_brace_pattern = "".join([seg.coded_brace_pattern for seg in segs])
--- ai_workspace_okapi/serializers.py
+++ ai_workspace_okapi/serializers.py
@@ -258,15 +258,15 @@ class MergeSegmentSerializer(serializers.ModelSerializer):
         segments = data["segments"] = sorted(data["segments"], key=lambda x: x.id)
 
         # Resetting the raw MT for normal segments once merged
-        merged_seg = ''
-        for segment in segments:
-            if segment.target or segment.temp_target:
-                merged_seg+=segment.target
-        if merged_seg != '':
-            segments[0].temp_target = merged_seg
-            segments[0].target = merged_seg
-            segments[0].save()
-            print("SEG[0]--------------->",segments[0])
+        # merged_seg = ''
+        # for segment in segments:
+        #     if segment.target or segment.temp_target:
+        #         merged_seg+=segment.target
+        # if merged_seg != '':
+        #     segments[0].temp_target = merged_seg
+        #     segments[0].target = merged_seg
+        #     segments[0].save()
+        #     print("SEG[0]--------------->",segments[0])
         #for segment in segments:
             # try:
             #     MT_RawTranslation.objects.get(segment_id = segment.id).delete()
