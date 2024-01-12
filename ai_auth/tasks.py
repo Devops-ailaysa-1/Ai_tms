3193f6a040a1828c8d544bb682326f7fdbcd8a55
ThenuVijay <warunvijay@gmail.com>
2024-01-12 12:17:48 +0530

    Merge target segments fix

--- ai_auth/tasks.py
+++ ai_auth/tasks.py
@@ -800,12 +800,12 @@ def mt_raw_update(task_id,segments):
     
     for seg in final_segments:###############Need to revise####################
         try:
-            if (type(seg) is Segment) or (type(seg) is MergeSegment):
+            if (type(seg) is Segment):# or (type(seg) is MergeSegment):
                 mt_raw = seg.seg_mt_raw
-            # if (type(seg) is Segment): 
-            #     mt_raw = seg.seg_mt_raw
-            # elif (type(seg) is MergeSegment):
-            #     mt_raw = seg.segments.first().seg_mt_raw
+            if (type(seg) is Segment): 
+                mt_raw = seg.seg_mt_raw
+            elif (type(seg) is MergeSegment):
+                mt_raw = seg.segments.first().seg_mt_raw
             else:
                 if seg.mt_raw_split_segment.exists() == False:
                     mt_raw = None
