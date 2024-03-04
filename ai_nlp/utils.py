e633a391162fe52bc352d221960e71c6759df776
Hemanthkumar2112 <hemanthmurugan21@gmail.com>
2024-03-04 17:41:19 +0530

    ner func added

--- ai_nlp/utils.py
+++ ai_nlp/utils.py
@@ -320,3 +320,23 @@ def extract_entities(sentence):
     #                               return_source_documents=True)
     # print("-------------------")
     # print(qa_chain(query) ) #chain.run(query).strip()
+
+
+import requests
+import os
+def ner_terminology_finder(file_path):
+    file_name = os.path.basename(file_path)
+
+    url = "https://transbuilderstaging.ailaysa.com/dataset/ner-upload/"
+
+    payload = {}
+    files=[
+    ('file',(file_name,open(file_path,'rb'),'text/plain'))]
+    headers = {}
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    if response.status_code == 200:
+        ner = response.json()['ner'].split(",")
+        terminology = response.json()['terminology'].split(",")
+        return {'ner':ner,'terminology':terminology}
+    else:
+        return None
