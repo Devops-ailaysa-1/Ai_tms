cef0e5d56e1fb0be02af47017428ba3f741a84fb
ThenuVijay <warunvijay@gmail.com>
2024-04-01 14:56:21 +0530

    unwanted code removed in ai_glex

--- ai_glex/urls.py
+++ ai_glex/urls.py
@@ -3,7 +3,7 @@ from . import api_views
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
-# router.register(r'glossary_list_create',api_views.GlossaryListCreateView, basename="glossary_list_create")
+
 router.register(r'glossary_file_upload',api_views.GlossaryFileView, basename="glossary_file_upload")
 router.register(r'term_upload',api_views.TermUploadView, basename="term_upload")
 router.register(r'glossary_selected',api_views.GlossarySelectedCreateView, basename='glossary-selected')
