d4ec515fff016809da6d0566f3bed9ab852469a1
Hemanthkumar2112 <hemanthmurugan21@gmail.com>
2024-08-29 17:28:39 +0530

    added model in admin

--- ai_auth/tasks.py
+++ ai_auth/tasks.py
@@ -844,8 +844,10 @@ def replace_mt_with_gloss(src,raw_mt,gloss , source_language , target_language )
         pr = prompt_phrase.format(tar_lang, src_lang, src, tar_lang, raw_mt,gloss, tar_lang)
         completion = openai.ChatCompletion.create(model=OPEN_AI_GPT_MODEL_REPLACE,messages=[{"role": "user", "content": pr}])
         res = completion["choices"][0]["message"]["content"]
-        lang_gram_prompt = LanguageGrammarPrompt.objects.get(language=target_language)
-        res = gemini_model_generative(lang_gram_prompt.prompt.format(res))
+        lang_gram_prompt = LanguageGrammarPrompt.objects.filter(language=target_language)
+        if lang_gram_prompt:
+            lang_gram_prompt = lang_gram_prompt.last()
+            res = gemini_model_generative(lang_gram_prompt.prompt.format(res))
         
 
         # Credit calculation
