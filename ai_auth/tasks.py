0328410fb178454757d176c13547870dc94be338
msharook <msharook@ailaysa.net>
2025-04-16 16:35:12 +0530

    Code Change

--- ai_auth/tasks.py
+++ ai_auth/tasks.py
@@ -1474,14 +1474,14 @@ def adaptive_segment_translation(segments_data, source_lang, target_lang, gloss_
     from ai_workspace_okapi.api_views import MT_RawAndTM_View
     from ai_workspace.api_views import UpdateTaskCreditStatus
     task = Task.objects.get(id=task_id)
-    # user = task.job.project.ai_user
-    # seg_ids = [segment["segment_id"] for segment in segments_data]
-    # consumable_credits = MT_RawAndTM_View.get_adaptive_consumable_credits_multiple_segments(task.document, seg_ids, None)
-    # if consumable_credits < user.credit_balance.get("total_left"):
-    #     UpdateTaskCreditStatus.update_credits(user, consumable_credits)
-    # else:
-    #     logger.info("Insufficient credits for segment translation")
-    #     raise ValueError("Insufficient credits for segment translation")
+    user = task.job.project.ai_user
+    seg_ids = [segment["segment_id"] for segment in segments_data]
+    consumable_credits = MT_RawAndTM_View.get_adaptive_consumable_credits_multiple_segments(task.document, seg_ids, None)
+    if consumable_credits < user.credit_balance.get("total_left"):
+        UpdateTaskCreditStatus.update_credits(user, consumable_credits)
+    else:
+        logger.info("Insufficient credits for segment translation")
+        raise ValueError("Insufficient credits for segment translation")
     try:
         translator = AdaptiveSegmentTranslator(source_lang, target_lang, os.getenv('ANTHROPIC_API_KEY') ,os.getenv('ANTHROPIC_MODEL_NAME'), gloss_terms)
         translated_segments = translator.process_batch(segments_data) 
