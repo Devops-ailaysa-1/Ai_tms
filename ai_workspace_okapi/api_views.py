647fb50283b5c6858091ff44ab588a0e00a2b7b6
JakeerHussainN <jakeer@ailaysa.net>
2025-04-16 16:16:06 +0530

    enhance code

--- ai_workspace_okapi/api_views.py
+++ ai_workspace_okapi/api_views.py
@@ -594,13 +594,13 @@ class SegmentsView(views.APIView, PageNumberPagination):
             else:
                 consumable_credits = 0
                 for seg in page_segments:
-                    if not seg.target:
+                    if not seg.temp_target:
                         consumable_credits += MT_RawAndTM_View.get_adaptive_consumable_credits(task.document, seg.id, None)
                 initial_credit = user.credit_balance.get("total_left")
                 if initial_credit < consumable_credits:
                     return  Response({'msg':'Insufficient Credits'}, status=400)
                 # No record found, so initiate a new translation task
-                if any(seg.target is None or seg.target == '' for seg in page_segments):
+                if any(seg.temp_target is None or seg.temp_target == '' for seg in page_segments):
                     page_segments_serialized = [{"id": seg.id} for seg in page_segments]
                     adaptive_translate.apply_async((task.id, page_segments_serialized), queue="high-priority")  
         segments_ser = SegmentSerializer(page_segments, many=True)
