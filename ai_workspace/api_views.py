5a64f0c3e3c042387ed2d03dd1f5fc747c502b0a
msharook <msharook@ailaysa.net>
2025-05-16 10:47:47 +0530

    ProgressUpdate

--- ai_workspace/api_views.py
+++ ai_workspace/api_views.py
@@ -5361,9 +5361,7 @@ class AdaptiveFileTranslate(viewsets.ViewSet):
 
                 for batch in batches:
                     task_result = TaskResult.objects.filter(task_id=batch.celery_task_id).first()
-                    progress_data = None
-                    if batch.status != 'FAILED':
-                        progress_data = self.get_progress(batch)
+                    progress_data = self.get_progress(batch)
                     if progress_data!=None:
                         total += progress_data.get('total', 0)
                     else:
