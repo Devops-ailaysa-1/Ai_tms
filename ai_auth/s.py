b21057bd9a39c4a334ae868c61373e5c6d0351e6
msharook <msharook@ailaysa.net>
2025-12-09 17:38:37 +0530

    CodeChange

--- ai_auth/s.py
+++
@@ -1,9 +0,0 @@
-a = """
-    asfasdfasdfasdf<br><br>adasdfasdfasdfasdfasdfa<br><br><br><br><br><br>asdfasdfasdfasdfasdfasfd
-"""
-
-res = a.split("<br><br>")
-for i in res:
-    if i.strip():
-        print(i)
-
