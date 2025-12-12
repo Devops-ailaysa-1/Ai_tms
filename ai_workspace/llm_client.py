02c14976bd7dde4b915d8496ad3ec1147991814e
Hemanthkumar2112 <hemanth@ailaysa.net>
2025-12-12 10:36:48 +0530

    change in the fix

--- ai_workspace/llm_client.py
+++ ai_workspace/llm_client.py
@@ -45,7 +45,7 @@ def is_numbers_or_punctuation(text: str) -> bool:
     return all(c in allowed for c in text)
 
 
-credentials_nebius = service_account.Credentials.from_service_account_file(AI_RESEARCH_VERTEX_AI_MODEL_LINK,scopes=["https://www.googleapis.com/auth/cloud-platform"])
+credentials_nebius = service_account.Credentials.from_service_account_file(AI_RESEARCH_VERTEX_AI_JSON_PATH,scopes=["https://www.googleapis.com/auth/cloud-platform"])
 
 safety_settings=[
             types.SafetySetting(
@@ -159,13 +159,15 @@ class LLMClient:
     
     @backoff.on_exception(backoff.expo, Exception, max_tries=3, jitter=backoff.full_jitter)
     def _handle_vertex_ai_pib(self, messages, system_instruction, max_tokens=60000):
+
+        print("clled vetex ai")
         
         if is_numbers_or_punctuation(messages):
-            return messages,0
+            return messages, 0
         
     
 
-        client = genai.Client(project = AI_RESEARCH_VERTEX_AI,  vertexai=True, location='us-central1',credentials = credentials_nebius )
+        client = genai.Client(project = AI_RESEARCH_VERTEX_AI,  vertexai=True, location=AI_RESEARCH_VERTEX_AI_LOCATION,credentials = credentials_nebius )
 
         generate_content_config = types.GenerateContentConfig(temperature = 1, top_p = 0.95,
                                                               system_instruction = system_instruction)
@@ -177,10 +179,8 @@ class LLMClient:
              full_text = ""
              if chunk.text:
                  full_text+=chunk.text
-             
- 
-        
-        return full_text
+    
+        return full_text,0
 
 
 
